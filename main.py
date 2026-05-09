20634fcfab5d6a3aa40de1d8f43a07a2481d4024
dev-karani <edwin.k.kithinji@gmail.com>
2026-05-09 12:14:28 +0300

    feat:validate schema using pydantic

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 #request Get method url "/"
-  
+class Post(BaseModel):
+    title: str
+    content:str
+    publish: bool = True #optional with default
+    rating: Optional[int] = None #optional 
+
 @app.get("/")
 def root():
     return {"message":"welcome to my final api"}
@@ -14,6 +21,7 @@ def get_posts():
     return {"data":"this is your posts"}
   
 @app.post("/createposts")
-def createposts(payload:dict=Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']}: content:{payload['content']}"}
+def createposts(post:Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
